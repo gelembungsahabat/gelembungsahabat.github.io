d41a11bdbfcc71181dd2adc67baeb6ff3a4fbc45
wildan <wildanrgsspeed@gmail.com>
2020-09-08 23:06:17 +0700

    alhamdulillaah add sidebar

--- components/navBar.vue
+++ components/navBar.vue
@@ -1,39 +1,73 @@
 <template>
-  <div class="navbar">
-
-    <!-- logo JDD -->
-    <div class="logo">
-      <a href="/">
-
-        <img
-          class="textlogo"
-          src="../static/WELLDONE.svg"
-          alt="text logo JDD"
-        ></a>
-    <ul>
-      <li>
-        <nuxt-link to="/about">
-          ABOUT
-        </nuxt-link>
-      </li>
-    </ul>
+  <div>
+    <div
+      id="mySidebar"
+      class="sidebar"
+    >
+      <a
+        href="javascript:void(0)"
+        class="closebtn"
+        @click="closeNav()"
+      >&times;</a>
+      <a href="#">About</a>
+      <a href="#">Services</a>
+      <a href="#">Clients</a>
+      <a href="#">Contact</a>
     </div>
 
+    <div class="navbar">
+
+      <!-- logo JDD -->
+      <div class="logo">
+        <a href="/">
+
+          <img
+            class="textlogo"
+            src="../static/WELLDONE.svg"
+            alt="text logo JDD"
+          ></a>
+        <ul>
+          <li>
+            <button
+              class="openbtn"
+              @click="openNav()"
+            >&#9776; Open Sidebar</button>
+
+          </li>
+        </ul>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+
+    openNav () {
+      document.getElementById("mySidebar").style.width = "250px";
+      // document.getElementById("main").style.marginRight = "250px";
+    },
+
+    /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+    closeNav () {
+      document.getElementById("mySidebar").style.width = "0";
+      // document.getElementById("main").style.marginRight = "0";
+    }
+  }
+}
+
 </script>
 
 <style>
 .navbar {
   position: fixed;
-  opacity: 0.8;
+  opacity: 1;
   background-color: #333;
   top: 0;
   left: 0;
-  z-index: 9899;
+  z-index: 1;
   width: 100%;
   height: 60px;
 }
@@ -61,7 +95,6 @@ li {
   @apply float-right;
 }
 
-
 li a {
   display: block;
   color: white;
@@ -72,6 +105,76 @@ li a {
 
 li a:hover:not(.active) {
   background-color: #111;
+}
+
+/* ini untuk sidebar */
+
+/* The sidebar menu */
+.sidebar {
+  height: 100%; /* 100% Full-height */
+  width: 0; /* 0 width - change this with JavaScript */
+  position: fixed; /* Stay in place */
+  z-index: 3; /* Stay on top */
+  top: 0;
+  right: 0;
+  background-color: #111; /* Black*/
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 60px; /* Place content 60px from the top */
+  transition: 0.5s; /* 0.5 second transition effect to slide in the sidebar */
+}
+
+/* The sidebar links */
+.sidebar a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+/* When you mouse over the navigation links, change their color */
+.sidebar a:hover {
+  color: #f1f1f1;
+}
+
+/* Position and style the close button (top right corner) */
+.sidebar .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+/* The button used to open the sidebar */
+.openbtn {
+  font-size: 20px;
+  cursor: pointer;
+  background-color: #111;
+  color: white;
+  padding: 10px 15px;
+  border: none;
+}
+
+.openbtn:hover {
+  background-color: #444;
+}
+
+/* Style page content - use this if you want to push the page content to the right when you open the side navigation */
+#main {
+  transition: margin-left 0.5s; /* If you want a transition effect */
+  padding: 20px;
+}
+
+/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+@media screen and (max-height: 450px) {
+  .sidebar {
+    padding-top: 15px;
+  }
+  .sidebar a {
+    font-size: 18px;
+  }
 }
 
 /* @screen sm {
@@ -91,5 +194,4 @@ li a:hover:not(.active) {
     @apply bg-green-500 h-12;
   }
 } */
-
 </style>
