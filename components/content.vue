e4805808907582bd8c946b0ac67ed4a48af1554c
wildan <wildanrgsspeed@gmail.com>
2020-09-24 23:48:47 +0700

    fixing raw card

--- components/content.vue
+++ components/content.vue
@@ -17,73 +17,85 @@
       </div>
     </nuxt-link>
 
-    <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5 ">
-      <img
-        src="../assets/images/mirai.jpg"
-        alt="mirai"
-        class="w-full"
-      >
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">
-          coba
+    <nuxt-link to="/about">
+      <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5">
+        <img
+          src="../assets/images/mirai.jpg"
+          alt="mirai"
+          class="w-full"
+        >
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">
+            coba
+          </div>
+          <p class="text-gray-700 text-base break-word">aku bingug kate lapo iki akuu bingung gingkjaksjd asdakh askdjahd askdha </p>
         </div>
-        <p class="text-gray-700 text-base"> wakkwakaksaksdjaksldjalksdjalksjdalksjdalksjdal</p>
-      </div>
-    </div>
 
-    <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5 ">
-      <img
-        src="../assets/images/mirai.jpg"
-        alt="mirai"
-        class="w-full"
-      >
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">
-          coba
-        </div>
-        <p class="text-gray-700 text-base"> wakkwakaksaksdjaksldjalksdjalksjdalksjdalksjdal</p>
       </div>
-    </div>
+    </nuxt-link>
+    <nuxt-link to="/about">
+      <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5">
+        <img
+          src="../assets/images/mirai.jpg"
+          alt="mirai"
+          class="w-full"
+        >
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">
+            coba
+          </div>
+          <p class="text-gray-700 text-base break-word">aku bingug kate lapo iki akuu bingung gingkjaksjd asdakh askdjahd askdha </p>
+        </div>
 
-    <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5 ">
-      <img
-        src="../assets/images/mirai.jpg"
-        alt="mirai"
-        class="w-full"
-      >
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">
-          coba
-        </div>
-        <p class="text-gray-700 text-base"> wakkwakaksaksdjaksldjalksdjalksjdalksjdalksjdal</p>
       </div>
-    </div>
-    <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5 ">
-      <img
-        src="../assets/images/mirai.jpg"
-        alt="mirai"
-        class="w-full"
-      >
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">
-          coba
+    </nuxt-link>
+    <nuxt-link to="/about">
+      <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5">
+        <img
+          src="../assets/images/mirai.jpg"
+          alt="mirai"
+          class="w-full"
+        >
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">
+            coba
+          </div>
+          <p class="text-gray-700 text-base break-word">aku bingug kate lapo iki akuu bingung gingkjaksjd asdakh askdjahd askdha </p>
         </div>
-        <p class="text-gray-700 text-base"> wakkwakaksaksdjaksldjalksdjalksjdalksjdalksjdal</p>
+
       </div>
-    </div>
-    <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5 ">
-      <img
-        src="../assets/images/mirai.jpg"
-        alt="mirai"
-        class="w-full"
-      >
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">
-          coba
+    </nuxt-link>
+    <nuxt-link to="/about">
+      <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5">
+        <img
+          src="../assets/images/mirai.jpg"
+          alt="mirai"
+          class="w-full"
+        >
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">
+            coba
+          </div>
+          <p class="text-gray-700 text-base break-word">aku bingug kate lapo iki akuu bingung gingkjaksjd asdakh askdjahd askdha </p>
         </div>
-        <p class="text-gray-700 text-base"> wakkwakaksaksdjaksldjalksdjalksjdalksjdalksjdal</p>
+
       </div>
-    </div>
+    </nuxt-link>
+    <nuxt-link to="/about">
+      <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5">
+        <img
+          src="../assets/images/mirai.jpg"
+          alt="mirai"
+          class="w-full"
+        >
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">
+            coba
+          </div>
+          <p class="text-gray-700 text-base break-word">aku bingug kate lapo iki akuu bingung gingkjaksjd asdakh askdjahd askdha </p>
+        </div>
+
+      </div>
+    </nuxt-link>
   </div>
-
 </template>
