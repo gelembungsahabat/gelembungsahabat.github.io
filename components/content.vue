2f608e30775d41f85cdccaf3c817c209fe8217b4
wildan <wildanrgsspeed@gmail.com>
2020-09-25 00:23:45 +0700

    fixing card content

--- components/content.vue
+++ components/content.vue
@@ -1,70 +1,70 @@
 <template>
   <div class="flex flex-wrap py-20 justify-center">
-    <nuxt-link to="/about">
+    <a href="https://medium.com/@gelembungsahabat/deteksi-warna-dengan-metode-image-thresholding-opencv-python-3-f3686de4308">
       <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5">
         <img
-          src="../assets/images/mirai.jpg"
-          alt="mirai"
+          src="../assets/images/imagethres.png"
+          alt="image thresholding"
           class="w-full"
         >
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">
-            coba
+            Image Thresholding with OpenCV Python3
           </div>
-          <p class="text-gray-700 text-base break-word">aku bingug kate lapo iki akuu bingung gingkjaksjd asdakh askdjahd askdha </p>
+          <p class="text-gray-700 text-base break-word">Pengenalan dasar image thresholding beserta source codenya</p>
         </div>
 
       </div>
-    </nuxt-link>
+    </a>
 
-    <nuxt-link to="/about">
+    <a href="https://medium.com/@gelembungsahabat/pengenalan-reactjs-40ffa31ae9c7">
       <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5">
         <img
-          src="../assets/images/mirai.jpg"
-          alt="mirai"
+          src="../assets/images/reactjs.png"
+          alt="pengenalan reactjs"
           class="w-full"
         >
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">
-            coba
+            Pengenalan ReactJS
           </div>
-          <p class="text-gray-700 text-base break-word">aku bingug kate lapo iki akuu bingung gingkjaksjd asdakh askdjahd askdha </p>
+          <p class="text-gray-700 text-base break-word">Pengenalan salah satu framework JS paling populer yaitu ReactJS </p>
         </div>
 
       </div>
-    </nuxt-link>
-    <nuxt-link to="/about">
+    </a>
+    <a href="https://medium.com/@gelembungsahabat/persiapan-membuat-website-dengan-reactjs-435ccac65c54">
       <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5">
         <img
-          src="../assets/images/mirai.jpg"
-          alt="mirai"
+          src="../assets/images/persiapanreact.png"
+          alt="persiapan bikin project reactjs"
           class="w-full"
         >
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">
-            coba
+            Persiapan Membuat Web dengan ReactJS
           </div>
-          <p class="text-gray-700 text-base break-word">aku bingug kate lapo iki akuu bingung gingkjaksjd asdakh askdjahd askdha </p>
+          <p class="text-gray-700 text-base break-word">Materi tentang setup project ReactJS beserta penjelasannya</p>
         </div>
 
       </div>
-    </nuxt-link>
-    <nuxt-link to="/about">
+    </a>
+    <a href="https://medium.com/@gelembungsahabat/belajar-membuat-static-site-menggunakan-reactjs-part1-4bb9a3a02307">
       <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5">
         <img
-          src="../assets/images/mirai.jpg"
-          alt="mirai"
+          src="../assets/images/staticwebreact1.png"
+          alt="bikin static web pake reactjs part 1"
           class="w-full"
         >
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">
-            coba
+            Belajar Membuat Static Site Menggunakan ReactJS #part1
           </div>
-          <p class="text-gray-700 text-base break-word">aku bingug kate lapo iki akuu bingung gingkjaksjd asdakh askdjahd askdha </p>
+          <p class="text-gray-700 text-base break-word">Belajar membuat skeleton atau kerangka dari project web dan belajar mengenai `Components`</p>
         </div>
 
       </div>
-    </nuxt-link>
+    </a>
     <nuxt-link to="/about">
       <div class="max-w-md my-5 rounded overflow-hidden shadow-lg mx-5">
         <img
