8cd8a9d836dd57d6e76fdd948a0f5d790f146c22
gelembungsahabat <wildanrgssport@gmail.com>
2025-05-07 13:09:37 +0700

    feat: add section bar

--- src/components/section-bar.tsx
+++ src/components/section-bar.tsx
@@ -1,12 +1,23 @@
 import "./section-bar.css";
+
+const Bar = (props: { section: number; recentSection: number }) => {
+  const { section, recentSection } = props;
+  return (
+    <div
+      className={
+        section === recentSection ? "section-bar-active" : "section-bar"
+      }
+    ></div>
+  );
+};
+
 export const SectionBar = (props: { section: number }) => {
   const { section } = props;
-  console.log(section, "section1");
   return (
-    <div className="section-bar">
-      <div hidden={section === 0}>1</div>
-      <div hidden={section === 1}>2</div>
-      <div hidden={section === 2}>3</div>
+    <div className="section-bar-wrapper">
+      <Bar section={0} recentSection={section} />
+      <Bar section={1} recentSection={section} />
+      <Bar section={2} recentSection={section} />
     </div>
   );
 };
