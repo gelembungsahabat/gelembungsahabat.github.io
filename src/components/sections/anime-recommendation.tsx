4713ef6d7a47057c3cf7fa3288c23ef954027ebf
gelembungsahabat <wildanrgssport@gmail.com>
2025-05-09 09:17:30 +0700

    feat: fix container for anime recommendation section

--- src/components/sections/anime-recommendation.tsx
+++ src/components/sections/anime-recommendation.tsx
@@ -1,22 +1,53 @@
 import "./styles/anime-recommendation.css";
 import { animeRecommendationData, type AnimeRecommendation } from "../../data";
+import { useState } from "react";
+
+const AnimeRecommendationItem = (props: {
+  index: number;
+  selectedAnimeIndex: number;
+  setSelectedAnimeIndex: (index: number) => void;
+  animeName: string;
+}) => {
+  const { index, selectedAnimeIndex, setSelectedAnimeIndex, animeName } = props;
+  return (
+    <div
+      className={
+        index === selectedAnimeIndex
+          ? "anime-list-item-active"
+          : "anime-list-item"
+      }
+      onClick={() => setSelectedAnimeIndex(index)}
+    >
+      {animeName}
+    </div>
+  );
+};
 
 export function AnimeRecommendation() {
+  const [selectedAnimeIndex, setSelectedAnimeIndex] = useState(0);
   return (
     <section className="anime-recommendation">
       <div>
         <h1>Anime Recommendation</h1>
       </div>
       <div className="anime-recommendation-wrapper">
-        {/* <div>
-          <h1>Anime Recommendation</h1>
-        </div> */}
         <div className="anime-list">
-          {animeRecommendationData.map((coba: AnimeRecommendation) => {
-            return <div className="anime-list-item"> {coba.name}</div>;
-          })}
+          {animeRecommendationData.map(
+            (anime: AnimeRecommendation, index: number) => {
+              return (
+                <AnimeRecommendationItem
+                  index={index}
+                  selectedAnimeIndex={selectedAnimeIndex}
+                  setSelectedAnimeIndex={setSelectedAnimeIndex}
+                  animeName={anime.name}
+                />
+              );
+            }
+          )}
         </div>
-        <div className="anime-details">coba</div>
+        <div className="anime-details">
+          {animeRecommendationData[selectedAnimeIndex].details}
+        </div>
       </div>
     </section>
   );
--- src/data/anime-recommendation-data.ts
+++ src/data/anime-recommendation-data.ts
@@ -6,42 +6,51 @@ export type AnimeRecommendation = {
 export const animeRecommendationData: AnimeRecommendation[] = [
   {
     name: "Witch Watch",
-    details: "hehe",
+    details: `Nico Wakatsuki has finished her six years of witch training and is moving back to town by herself. 
+    As a new witch, she must choose a familiar to accompany her. However, unlike the other witches, she wishes to pick a 
+    non-animal familiar. There's no better companion than Morihito "Moi" Otogi—her childhood friend who happens to be an 
+    ogre. Morihito is vehemently opposed to being her servant, but he reluctantly accepts his fate after learning that a 
+    curse would revert him into a strange cow-tiger hybrid were he to disobey a witch's order.Due to Morihito's father 
+    leaving for a business trip and Nico's mother remaining behind in the Witches' Holy Land, the two teens 
+    find themselves living together under the same roof. While Nico is ecstatic to be living with her crush, 
+    Morihito finds that her magical antics and overall clumsiness create more problems than solutions. With a prophesied 
+    disaster to befall Nico within the year, Morihito must do everything he can to protect her as her bodyguard while 
+    also upholding his duties as her familiar.`,
   },
   {
     name: "Ousama Ranking",
-    details: "hehe",
+    details: "Details2",
   },
   {
     name: "Death Note",
-    details: "hehe",
+    details: "Details3",
   },
   {
     name: "Vivy: Flourite Eye's Song",
-    details: "hehe",
+    details: "Details4",
   },
   {
     name: "Code Geass: Hangyaku no Lelouch",
-    details: "hehe",
+    details: "Details5",
   },
   {
     name: "Makeine: Too Many Losing Heroines!",
-    details: "hehe",
+    details: "Details6",
   },
   {
     name: "One Punch Man (Season 1 GOATED)",
-    details: "hehe",
+    details: "Details7",
   },
   {
     name: "One Piece Fan Letter",
-    details: "hehe",
+    details: "Details8",
   },
   {
     name: "Fullmetal Alchemist: Brotherhood",
-    details: "hehe",
+    details: "Details9",
   },
   {
     name: "[Oshi no Ko]",
-    details: "hehe",
+    details: "Details10",
   },
 ];
