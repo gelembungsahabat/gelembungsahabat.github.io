b986ce9e58f6150dcfbb6a2191062428060a0f69
gelembungsahabat <wildanrgssport@gmail.com>
2025-05-23 08:24:19 +0700

    add some bg images for anime details

--- src/data/anime-recommendation-data.ts
+++ src/data/anime-recommendation-data.ts
@@ -14,32 +14,36 @@ export const animeRecommendationData: AnimeRecommendation[] = [
   },
   {
     name: "Ousama Ranking",
-    details: "Details2",
-    imgUrl: "/images/witch-watch.png",
-    genre: "Comedy, Supernatural",
+    details: `The people of the kingdom look down on the young Prince Bojji, who can neither hear nor speak. They call him "The Useless Prince" while jeering at his supposed foolishness.
+
+However, while Bojji may not be physically strong, he is certainly not weak of heart. When a chance encounter with a shadow creature should have left him traumatized, it instead makes him believe that he has found a friend amidst those who only choose to notice his shortcomings. He starts meeting with Kage, the shadow, regularly, to the point where even the otherwise abrasive creature begins to warm up to him.
+
+Kage and Bojji's unlikely friendship lays the budding foundations of the prince's journey, one where he intends to conquer his fears and insecurities. Despite the constant ridicule he faces, Bojji resolves to fulfill his desire of becoming the best king he can be.`,
+    imgUrl: "/images/ousama-ranking.jpg",
+    genre: "Adventure, Fantasy",
   },
   {
     name: "Death Note",
     details: "Details3",
-    imgUrl: "/images/witch-watch.png",
+    imgUrl: "/images/death-note.jpg",
     genre: "Comedy, Supernatural",
   },
   {
     name: "Vivy: Flourite Eye's Song",
     details: "Details4",
-    imgUrl: "/images/witch-watch.png",
+    imgUrl: "/images/vivy.jpg",
     genre: "Comedy, Supernatural",
   },
   {
     name: "Code Geass: Hangyaku no Lelouch",
     details: "Details5",
-    imgUrl: "/images/witch-watch.png",
+    imgUrl: "/images/code-geass.png",
     genre: "Comedy, Supernatural",
   },
   {
     name: "Makeine: Too Many Losing Heroines!",
     details: "Details6",
-    imgUrl: "/images/witch-watch.png",
+    imgUrl: "/images/makeine.jpg",
     genre: "Comedy, Supernatural",
   },
   {
