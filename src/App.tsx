9b8f9283dc8bcbbf5eb2cb95bc8d26581f32c317
gelembungsahabat <wildanrgssport@gmail.com>
2025-06-04 08:22:12 +0700

    feat: add arrow key scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import { SectionBar } from "./components/section-bar";
 import { Hero } from "./components/sections/hero";
 import { AnimeRecommendation } from "./components/sections/anime-recommendation";
 import { FunThings } from "./components/sections/fun-things";
-// import { Articles } from "./components/sections/articles";
-import { usePreventKeyboardScrolling, usePreventMousewheelZoom } from "./hooks";
+import { usePreventMousewheelZoom } from "./hooks";
 
 function App() {
   const [currentSection, setCurrentSection] = useState(0);
@@ -21,19 +20,35 @@ function App() {
 
   useEffect(() => {
     const sections = document.querySelectorAll("section");
+
+    // Arrow key scrolling
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (["ArrowUp"].includes(e.code) && currentSection >= 1) {
+        e.preventDefault();
+        setCurrentSection((prevState) => prevState - 1);
+      }
+      if (["ArrowDown"].includes(e.code) && currentSection < 2) {
+        e.preventDefault();
+        setCurrentSection((prevState) => prevState + 1);
+      }
+    };
+    window.addEventListener("keydown", onKeyDown, false);
+
+    // Scroll to section
     setTimeout(() => {
       sections[currentSection].scrollIntoView({ behavior: "smooth" });
     }, 100);
-  }, [currentSection]);
+
+    // Prevent KeyDown being triggered 2 times
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, [currentSection, setCurrentSection]);
 
   usePreventMousewheelZoom();
-  usePreventKeyboardScrolling();
 
   return (
     <>
       <div className="homepage" onWheel={scroll}>
         <Hero setCurrentSection={setCurrentSection} />
-        {/* <Articles /> */}
         <AnimeRecommendation />
         <FunThings />
       </div>
