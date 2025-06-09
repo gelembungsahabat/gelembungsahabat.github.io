5f1ce688dab2ccbbe4c0517261cf43725c9642a5
gelembungsahabat <wildanrgssport@gmail.com>
2025-06-09 09:16:15 +0700

    feat: add pixel art gif on fun things page

--- src/components/sections/fun-things.tsx
+++ src/components/sections/fun-things.tsx
@@ -4,14 +4,26 @@ import { IoWarningOutline } from "react-icons/io5";
 export function FunThings() {
   return (
     <section className="fun-things">
-      <div className="flex-container">
-        <div>
-          <h1>Fun Things Section</h1>
-          <h2>Raw Fun Things Section</h2>
-          <div className="warning-container">
-            <IoWarningOutline />
-            <p>This Website is under development</p>
+      <div className="fun-things-container">
+        <div className="flex-container">
+          <div>
+            <h1>Fun Things Section</h1>
+            <h2>Raw Fun Things Section</h2>
+            <div className="warning-container">
+              <IoWarningOutline />
+              <p>This Website is under development</p>
+            </div>
           </div>
+          <img
+            src="/images/itachivsasuke.gif"
+            alt="itachigif"
+            style={{
+              imageRendering: "pixelated",
+              width: "50rem",
+              height: "50rem",
+              marginRight: "20rem",
+            }}
+          />
         </div>
       </div>
     </section>
--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -5,43 +5,45 @@ export function Hero(props: { setCurrentSection: (section: number) => void }) {
   const { setCurrentSection } = props;
   return (
     <section className="hero">
-      <div className="flex-container">
-        <div>
-          <h1>Hi, I'm Muhammad Wildan</h1>
-          <h2>Welcome to my personal blog!</h2>
-          <div className="warning-container">
-            <IoWarningOutline />
-            <p>This Website is under development</p>
+      <div className="hero-page-container">
+        <div className="flex-container">
+          <div>
+            <h1>Hi, I'm Muhammad Wildan</h1>
+            <h2>Welcome to my personal blog!</h2>
+            <div className="warning-container">
+              <IoWarningOutline />
+              <p>This Website is under development</p>
+            </div>
           </div>
-        </div>
-        <div className="card-container">
-          <a
-            href="https://github.com/gelembungsahabat?tab=repositories"
-            target="_blank"
-            className="card has-bg-img project-list-img"
-          >
-            <div className="card-title-container">
-              <span className="card-title">Project List</span>
-            </div>
-          </a>
-          <a
-            className="card has-bg-img anime-recommendation-img"
-            onClick={() => setCurrentSection(1)}
-            tabIndex={0}
-          >
-            <div className="card-title-container">
-              <span className="card-title">Anime Recommendation</span>
-            </div>
-          </a>
-          <a
-            className="card has-bg-img fun-things-img"
-            onClick={() => setCurrentSection(2)}
-            tabIndex={0}
-          >
-            <div className="card-title-container">
-              <span className="card-title">Fun Things</span>
-            </div>
-          </a>
+          <div className="card-container">
+            <a
+              href="https://github.com/gelembungsahabat?tab=repositories"
+              target="_blank"
+              className="card has-bg-img project-list-img"
+            >
+              <div className="card-title-container">
+                <span className="card-title">Project List</span>
+              </div>
+            </a>
+            <a
+              className="card has-bg-img anime-recommendation-img"
+              onClick={() => setCurrentSection(1)}
+              tabIndex={0}
+            >
+              <div className="card-title-container">
+                <span className="card-title">Anime Recommendation</span>
+              </div>
+            </a>
+            <a
+              className="card has-bg-img fun-things-img"
+              onClick={() => setCurrentSection(2)}
+              tabIndex={0}
+            >
+              <div className="card-title-container">
+                <span className="card-title">Fun Things</span>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </section>
