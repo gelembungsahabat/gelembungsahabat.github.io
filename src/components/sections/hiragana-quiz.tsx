09ad53f155840656d38f333d4122b9bf43e6e2d7
gelembungsahabat <wildanrgssport@gmail.com>
2025-06-23 23:18:25 +0700

    feat: change bg color if input is false

--- src/components/sections/hiragana-quiz.tsx
+++ src/components/sections/hiragana-quiz.tsx
@@ -1,5 +1,6 @@
 import "./styles/hiragana-quiz.css";
 import { hiraganaData } from "../../data/hiragana-data";
+import { useState, type ChangeEvent } from "react";
 
 function getRandomObjectEntries(obj: object, n: number) {
   const entries = Object.entries(obj);
@@ -11,16 +12,39 @@ function getRandomObjectEntries(obj: object, n: number) {
 const randomSubset = getRandomObjectEntries(hiraganaData, 15);
 
 export function FunThings() {
+  const [myStyle, setMyStyle] = useState({});
+  const changeBackground = (id: number) => {
+    setMyStyle((prevState: string) => ({
+      ...myStyle,
+      [id]: !prevState[id],
+    }));
+  };
+
   return (
     <section className="hiragana-quiz">
       <div>
         <h1>Random Hiragana (ひらがな) Quiz</h1>
       </div>
       <div className="hiragana-quiz-container">
-        {Object.entries(randomSubset).map(([key]) => (
-          <div className="hiragana-container" tabIndex={0}>
+        {Object.entries(randomSubset).map(([key, answer], index) => (
+          <div
+            className="hiragana-container"
+            key={index}
+            style={{ backgroundColor: myStyle[`${index}`] ? "red" : "blue" }}
+          >
             <h1 className="hiragana">{key}</h1>
-            <input type="text" />
+            <input
+              type="text"
+              onBlur={(e: ChangeEvent<HTMLInputElement>) => {
+                const inputValue = e.target.value;
+                if (inputValue !== answer) {
+                  changeBackground(index);
+                }
+                if (inputValue === answer && myStyle[`${index}`]) {
+                  changeBackground(index);
+                }
+              }}
+            />
           </div>
         ))}
       </div>
