672baac19dcf5dd5152a8c4efc2dcb3d3dc23be6
gelembungsahabat <wildanrgssport@gmail.com>
2025-07-21 09:10:53 +0700

    feat: add raw container on articles section

--- src/components/sections/articles.tsx
+++ src/components/sections/articles.tsx
@@ -4,9 +4,9 @@ import { IoWarningOutline } from "react-icons/io5";
 export function Articles() {
   return (
     <section className="articles">
+      <h1>Articles Section</h1>
       <div className="articles-container">
         <div>
-          <h1>Articles Section</h1>
           <h2>Raw Articles Section</h2>
           <div className="warning-container">
             <IoWarningOutline />
