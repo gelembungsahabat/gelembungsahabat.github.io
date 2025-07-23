c7f28f4406ee473901fc87734788dee01bcba120
gelembungsahabat <wildanrgssport@gmail.com>
2025-07-23 16:32:27 +0700

    feat: test with react-snap

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,23 @@ import App from "./App.tsx";
 import { IconContext } from "react-icons";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { InstallWSL } from "./components/pages/install-wsl.tsx";
+import { hydrateRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <IconContext.Provider value={{ size: "1.4rem", className: "icon" }}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <Routes>
-          <Route index element={<App />} />
-          <Route path="/install-wsl" element={<InstallWSL />} />
-        </Routes>
-      </BrowserRouter>
-    </IconContext.Provider>
-  </StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement!.hasChildNodes()!) {
+  hydrateRoot(rootElement!, <App />);
+} else {
+  createRoot(rootElement!).render(
+    <StrictMode>
+      <IconContext.Provider value={{ size: "1.4rem", className: "icon" }}>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <Routes>
+            <Route index element={<App />} />
+            <Route path="/install-wsl" element={<InstallWSL />} />
+          </Routes>
+        </BrowserRouter>
+      </IconContext.Provider>
+    </StrictMode>
+  );
+}
