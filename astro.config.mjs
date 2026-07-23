6d6d4f5cd98d4bfb255fa2d379cef4bf55fe1df5
gelembungsahabat <wildanrgssport@gmail.com>
2026-07-23 17:19:41 +0700

    fix: fix build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({ mode: 'standalone' }),
+  site: 'https://gelembungsahabat.github.io',
+  output: 'static',
   integrations: [react()]
 });
